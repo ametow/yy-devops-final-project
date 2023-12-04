c6e8ad53a5f677eb323acb0da084bfcadcfe145a
Arslan Ametov <arsinux@yandex.com>
2023-12-04 19:12:09 +0500

    tuned monitoring bingo service, haproxy cfg

--- bingo/monitor_bingo.sh
+++ bingo/monitor_bingo.sh
@@ -4,11 +4,13 @@ SERVICE_URL="http://localhost:15466/ping"
 
 COMMAND_TO_EXECUTE="sudo systemctl restart bingo"
 
-CHECK_INTERVAL=31
+CHECK_INTERVAL=10
 
 check_service() {
     RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" $SERVICE_URL)
-    if [ "$RESPONSE" != "200" ]; then
+    if [ $? -ne 0 ]; then
+        echo "Connection refused, ignoring..."
+    elif [ $RESPONSE -ne 200 ]; then
         echo "Service is not healthy. Executing command..."
         eval $COMMAND_TO_EXECUTE
     fi
